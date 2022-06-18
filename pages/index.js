433cf2334482c8cfd9ca623dc0e7c217b220774c
route-2 <ruthurao@gmail.com>
2022-06-18 19:21:54 +0530

    enter

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,27 @@ import Link from 'next/link'
 import Image from 'next/image'
 import {Flex,Box,Text,Button} from '@chakra-ui/react'
 
+
+
+
+const Banner = (purpose) => (
+<Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10"> 
+
+{purpose}
+
+
+</Flex>
+
+
+
+)
+
 export default function Home() {
   return (
     <div className={styles.container}>
-      
+      <Banner purpose={'For Sale'}/>
+      <Banner purpose={'For Rent'}/>
+
     </div>
   )
 }
