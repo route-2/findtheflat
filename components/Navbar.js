80437380b535bbf9f5d7f3685776eeb8db9d5001
route-2 <ruthurao@gmail.com>
2022-06-20 02:48:25 +0530

    update

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,25 +1,27 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import React from 'react';
+import { PhoneIcon,CheckIcon } from '@chakra-ui/icons';
 import { useDisclosure,useState } from '@chakra-ui/react';
-import { ModalFooter,InputGroup,InputLeftAddon,InputRightAddon,PinInputField,PinInput,ModalHeader,ModelContent,Modal,ModalBody,ModalOverlay } from "@chakra-ui/react";
+import { ModalFooter,InputLeftElement,InputRightElement, InputGroup,ModalContent,InputLeftAddon,InputRightAddon,PinInputField,PinInput,ModalHeader,ModalCloseButton,Modal,ModalBody,ModalOverlay } from "@chakra-ui/react";
 import { Flex, Box, Text,Input, Button,Heading } from '@chakra-ui/react';
 const Navbar = () => {
-  // const OverlayTwo = () => (
-  //   <ModalOverlay
-  //     bg='none'
-  //     backdropFilter='auto'
-  //     backdropInvert='80%'
-  //     backdropBlur='2px'
-  //   />
-  // )
-  // const OverlayOne = () => (
-  //   <ModalOverlay
-  //     bg='blackAlpha.300'
-  //     backdropFilter='blur(10px) hue-rotate(90deg)'
-  //   />
-  // )
-  // const [overlay, setOverlay] = useState()
-  // const { isOpen, onOpen, onClose } = useDisclosure()
+  const OverlayTwo = () => (
+    <ModalOverlay
+      bg='none'
+      backdropFilter='auto'
+      backdropInvert='80%'
+      backdropBlur='2px'
+    />
+  )
+  const OverlayOne = () => (
+    <ModalOverlay
+      bg='blackAlpha.300'
+      backdropFilter='blur(10px) hue-rotate(90deg)'
+    />
+  )
+  const [overlay, setOverlay] = React.useState(<OverlayOne/>)
+  const { isOpen, onOpen, onClose } = useDisclosure()
     return (
 <> 
 <Flex justifyContent={"space-between"} flexDirection={"row"}     
@@ -33,32 +35,36 @@ const Navbar = () => {
 <Button
           ml='4'
           onClick={() => {
-            setOverlay(<OverlayTwo />)
+            setOverlay(<OverlayOne />)
             onOpen()
           }}
         >
          Login / SignUp
         </Button>
-        {/* <Modal isCentered isOpen={isOpen} onClose={onClose}>
+        <Modal isCentered isOpen={isOpen} onClose={onClose}>
           {overlay}
           <ModalContent>
             <ModalHeader>Login/SignUp</ModalHeader>
             <ModalCloseButton />
             <ModalBody>
             <InputGroup>
-    <InputLeftAddon children="+91" />
-    <Input type="phone" borderLeftRadius="0" placeholder="phone number" />
+          
+           <InputLeftElement
+      pointerEvents="none"
+      children={<PhoneIcon color="gray.300" />}
+    />
+    <Input type="phone" placeholder="Phone number" />
   </InputGroup>
             
               
-<Button bg={'purple.200'} borderRadius={'22px'}> Send OTP  </Button>
+<Button bg={'pink.100'} mt={'40px'} borderRadius={'22px'}> Send OTP  </Button>
             </ModalBody>
             <ModalFooter>
               <Button onClick={onClose}>Close</Button>
             </ModalFooter>
           </ModalContent>
         </Modal>
- */}
+         
 
 
 </Box>
